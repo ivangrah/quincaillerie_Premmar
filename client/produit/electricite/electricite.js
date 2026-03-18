935614d460199283ab604005a546523d99d770db
grah desire <desirejeanivangrah@gmail.com>
2026-03-18 21:28:36 +0000

    brnach

--- client/produit/electricite/electricite.js
+++ client/produit/electricite/electricite.js
@@ -14,23 +14,41 @@ const description = [
     "Disjoncteur & Dominos"
 ];
 
+const lien = [
+    "proelect.html",
+    "rallonge.html",
+    "coffret.html",
+    "Eclair.html",
+    "dijonct.html",
+    
+];
+
 let index = 0;
 const img = document.getElementById("image");
 const texte = document.getElementById("texte");
 const boutav = document.getElementById("boutav");
 const boutar = document.getElementById("boutar");
+const liens = document.getElementById("liens");
 
 img.src = image[index];
 texte.textContent = description[index];
+liens.href = lien[index];
+
+
 
 boutav.addEventListener("click", function(){
     index = (index + 1) % image.length;
     img.src = image[index];
     texte.textContent = description[index];
+    liens.href = lien[index];
+
 });
 
 boutar.addEventListener("click", function(){
+
     index = (index - 1 + image.length) % image.length;
     img.src = image[index];
     texte.textContent = description[index];
+    liens.href = lien[index];
+
 });
